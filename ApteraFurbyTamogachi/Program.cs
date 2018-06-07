e9dd6c61b9d9d48676f26d9ae5f7834a9cd01700
ldohse <ldohse@FWA-SOF-032>
2018-06-07 08:26:53 -0400

    POC

--- ApteraFurbyTamogachi/Program.cs
+++ ApteraFurbyTamogachi/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ApteraFurbyTamogachi
 {
     class Program
     {
+        private static string command;
+        private static Tamogachi myFurby;
+
         static void Main(string[] args)
         {
+            command = "";
+            myFurby = new Tamogachi();
+            myFurby.Health = 10;
+            Clock();
+            while (command != "stop")
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "feed":
+                        Console.WriteLine(myFurby.Feed());
+                        break;
+                    case "health":
+                        Console.WriteLine(myFurby.GetHealth());
+                        break;
+                    case "status":
+                        Console.WriteLine(myFurby.GetStatus());
+                        break; 
+                    case "kill":
+                        myFurby.Kill();
+                        break;
+                    default:
+                        myFurby.Ignore();
+                        break;
+                }
+
+                Console.WriteLine($"Furby's Health: {myFurby.Health}");
+                if (myFurby.Health == 0)
+                {
+                    Console.Beep(); 
+                    Console.WriteLine("You killed Furby, you monster");
+                }
+
+            }
+        }
+
+        async static void Clock() {
+            while (myFurby.Health > 0) {
+                myFurby.Ignore();
+                await Task.Delay(3000);
+            }
         }
     }
 }
---
+++ ApteraFurbyTamogachi/Tamogachi.cs
@@ -0,0 +1,51 @@
+﻿namespace ApteraFurbyTamogachi
+{
+    public class Tamogachi
+    {
+        public int Health { get; set; }
+
+        public string Feed() {
+            Health++;
+            return $"Furby Love Food! \nFurby's Health is now at {Health}";
+        }
+
+        public void Ignore() {
+            Health--; 
+        }
+
+        public string GetHealth()
+        {
+            return $"Furby's health is: {Health}"; 
+        }
+
+        public string GetStatus() {
+            if (Health > 10)
+            {
+                return "Furby is doing amazing!";
+            }
+            else if (Health <= 10 && Health >= 8)
+            {
+                return "Furby is feeling pre-tty good.";
+            }
+            else if (Health <= 7 && Health >= 5)
+            {
+                return "Furby is OK.";
+            }
+            else if (Health <= 4 && Health >= 2)
+            {
+                return "Furby has seen better days. ";
+            }
+            else if (Health == 1) {
+                return "Please take care of Furby."; 
+            }
+            else {
+                return "..."; 
+            }
+        }
+
+        public void Kill()
+        {
+            Health = 0; 
+        }
+    }
+}
